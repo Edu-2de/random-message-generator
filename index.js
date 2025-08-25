d9c3cfb1a529573f7e741bf8f1583911e31d498d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-25 13:17:56 -0300

    Refactor startMenu function to streamline message selection and improve code readability

--- index.js
+++ index.js
@@ -52,69 +52,33 @@ function startMenu(){
   let shouldContinue = true;
 
   const loop = () => {
-    showMenu();
-    rl.question("Chose: ", (answer_type) => {
-      if(answer_type === '1'){
-        rl.question("Type your name: ",(name) => {
-          showMenuMessages();
-          rl.question("Chose: ", (answer) => {
-            switch(answer){
-              case "1":
-                console.log(generateMessage(name, astrologyActions, astrologyPlaces));
-                break;
-              case "2":
-                console.log(generateMessage(name, motivationalActions, motivationalPlaces));
-                break;
-              case "3":
-                console.log(generateMessage(name, jokeActions, jokePlaces));
-                break;
-              case "4":
-                console.log(generateMessage(name, movieActions, moviePlaces));
-                break;
-              case "0":
-                console.log("exit...");
-                shouldContinue = false;
-                rl.close();
-                return;
-              default:
-                console.log("Invalid!");
-            }
-            if (shouldContinue) loop();
-          })
-          loop();
-        })
-
-      }else if(answer_type === '2'){
-        showMenuMessages();
-        rl.question("Chose: ", (answer) => {
-          switch(answer){
-            case "1":
-              console.log(generateMessage(astrologySubjects, astrologyActions, astrologyPlaces));
-              break;
-            case "2":
-              console.log(generateMessage(motivationalSubjects, motivationalActions, motivationalPlaces));
-              break;
-            case "3":
-              console.log(generateMessage(jokeSubjects, jokeActions, jokePlaces));
-              break;
-            case "4":
-              console.log(generateMessage(movieSubjects, movieActions, moviePlaces));
-              break;
-            case "0":
-              console.log("exit...");
-              shouldContinue = false;
-              rl.close();
-              return;
-            default:
-              console.log("Invalid!");
-          }
-          if (shouldContinue) loop();
-        })
-        loop();
+    showMenuMessages();
+    rl.question("Chose: ", (answer) => {
+      switch(answer){
+        case "1":
+          console.log(generateMessage(astrologySubjects, astrologyActions, astrologyPlaces));
+          break;
+        case "2":
+          console.log(generateMessage(motivationalSubjects, motivationalActions, motivationalPlaces));
+          break;
+        case "3":
+          console.log(generateMessage(jokeSubjects, jokeActions, jokePlaces));
+          break;
+        case "4":
+          console.log(generateMessage(movieSubjects, movieActions, moviePlaces));
+          break;
+        case "0":
+          console.log("exit...");
+          shouldContinue = false;
+          rl.close();
+          return;
+        default:
+          console.log("Invalid!");
       }
-    });
+      if (shouldContinue) loop();
+    })
   }
-
+  loop();
 }
 
 startMenu();
